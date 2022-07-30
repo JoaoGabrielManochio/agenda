3f7f8785c3a77e9337c9a98eedc52f050b5a1bbc
João Gabriel <joao.gabriel@mazzafc.tech>
2022-07-30 13:33:02 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::get('/ping', function () {
+        return response()->json(['now' => date('Ymd H:i:s')]);
+    });
+
+    Route::post('token', 'API\UserController@getToken');
+
+    Route::group(['middleware' => ['auth:api']], function () {
+        Route::get('doctors', 'API\DoctorController@index');
+    });
+});
